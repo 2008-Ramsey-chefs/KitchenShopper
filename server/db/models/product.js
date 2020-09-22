e2b8fce8596e28b63eb2bc9561a0da68d4e935bb
Nicholas Geismar <nicholasgeismar@Macs-MBP.fios-router.home>
2020-09-22 17:28:48 -0400

    feat: added product, order, and updated user models

---
+++ server/db/models/product.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  itemName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSIxvUKuwFlM_CaJl4X_SD_uBVxFNIUD7dj9kbAnXJy93RDl4f9koE&usqp=CAc'
+  }
+})
+
+module.exports = Product
