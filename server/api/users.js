2caed4aafcf41e8443e8d492f94c99ba563d45a6
Nicholas Geismar <ncgeismar@gmail.com>
2020-09-23 17:36:54 -0400

    feat: finished up api/users routes

--- server/api/users.js
+++ server/api/users.js
@@ -19,78 +19,106 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:userId', async(req, res, next) =>{
-  try{
-    const userId = req.params.userId;
+//GET api/users/:userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
     const user = await User.findOne({
       attributes: ['id', 'email', 'imageUrl'],
       where: {
         id: userId
       }
     })
-    if(user){
-      res.json(user);
-    } else{
-      res.sendStatus(404);
+    if (user) {
+      res.json(user)
+    } else {
+      res.sendStatus(404)
     }
-  }catch(err){
+  } catch (err) {
     next(err)
   }
 })
 
-router.post('/', async(req, res, next) =>{
-  try{
+//POST api/users/
+router.post('/', async (req, res, next) => {
+  try {
     const {email, password, googleId, imageUrl} = req.body
     const user = await User.create({
       email,
       password,
       googleId,
       imageUrl
-    });
-    res.status(201).send(user);
-  }catch(err){
+    })
+    res.status(201).send(user)
+  } catch (err) {
     next(err)
   }
 })
 
-router.delete('/:userId', isAdmin, async(req, res, next) =>{
-  try{
-    const userId = req.params.userId;
+//DELETE api/users/:userId
+router.delete('/:userId', isAdmin, async (req, res, next) => {
+  try {
+    const userId = req.params.userId
     const user = await User.findOne({
       where: {
         id: userId
       }
     })
-    if(user){
-      await user.destroy(req.body);
-      res.sendStatus(204);
-    } else{
-      res.sendStatus(404);
+    if (user) {
+      await user.destroy(req.body)
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(404)
     }
-  }catch(err){
+  } catch (err) {
     next(err)
   }
 })
 
-router.put('/:userId', isValid, async(req, res, next) =>{
-  try{
-    const {password, imageUrl} = req.body;
-    const userId = req.params.userId;
+//PUT api/users/:userId
+router.put('/:userId', isValid, async (req, res, next) => {
+  try {
+    const {password, imageUrl} = req.body
+    const userId = req.params.userId
     const user = await User.findOne({
       where: {
         id: userId
       }
-    });
-    if(user){
+    })
+    if (user) {
       const updatedUser = await user.update({
         password,
         imageUrl
       })
-      res.json(updatedUser);
+      res.json(updatedUser)
     }
-  }catch(err){
+  } catch (err) {
     next(err)
   }
 })
 
+//PUT api/users/login
+router.put('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+        password: req.body.password
+      }
+    })
+    if (!user) {
+      res.sendStatus(401)
+    } else {
+      req.session.userId = user.id
+      res.json(user)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
+//DELETE api/users/logout
+router.delete('/logout', (req, res) => {
+  delete req.session.userId
+  res.sendStatus(204)
+})
