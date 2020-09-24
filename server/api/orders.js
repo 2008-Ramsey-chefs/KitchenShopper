1dcdfc79e455808a09e97f8f55c49fd6473b5925
Nicholas Geismar <ncgeismar@gmail.com>
2020-09-24 16:36:05 -0400

    feat: eager loaded most recent update for orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,2 +1,33 @@
 const router = require('express').Router()
-const {Order} = require('../db/models')
+const {Order, User, Product, Orderproduct} = require('../db/models')
+
+//GET /api/orders/
+// router.get('/', async (req, res, next) =>{
+//   const order = await Order.findOne({
+//     where: {
+//       userId: req.session.passport.user
+//     }
+//   })
+//   const cart = await Orderproduct.findAll({
+//     where: {
+//       orderId: order.id
+//     },
+//   })
+//   res.json(cart)
+// })
+
+//PUT api/orders/cart
+router.get('/cart', async (req, res, next) => {
+  const lastOrder = await Order.findOne({
+    order: [['updatedAt', 'DESC']]
+  })
+  // order: [
+  //   ["__createdAt", "DESC"],
+  // ]
+  // const cart = await Order.findOrCreate({where: {
+  //   userId: req.session.passport.user
+  // }})
+  res.send(lastOrder)
+})
+
+module.exports = router
