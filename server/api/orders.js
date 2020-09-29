77f9e9424aa6947c8aa1d3e9f99c4d4e939f1bf3
Namratha PM <namrathasmacbookpro@Namrathas-MacBook-Pro.local>
2020-09-29 16:08:06 -0500

    added quantity in single product view

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,23 +3,26 @@ const {Order, User, Product, Orderproduct} = require('../db/models')
 
 // /api/orders/checkout
 router.put('/checkout', async (req, res, next) => {
-  const lastOrder = await Order.findOne({
-    where: {
-      userId: req.session.passport.user,
-      orderPlaced: false
-    },
-    order: [['updatedAt', 'DESC']]
-  })
-  await lastOrder.update({
-    orderPlaced: true
-  })
-  res.sendStatus(200)
+  try {
+    const lastOrder = await Order.findOne({
+      where: {
+        userId: req.session.passport.user,
+        orderPlaced: false
+      },
+      order: [['updatedAt', 'DESC']]
+    })
+    await lastOrder.update({
+      orderPlaced: true
+    })
+    res.sendStatus(200)
+  } catch (error) {
+    next(error)
+  }
 })
 
 //PUT api/orders/createcart
 router.put('/createcart', async (req, res, next) => {
   try {
-    console.log('trying to create cart')
     const lastOrder = await Order.findOne({
       where: {
         userId: req.session.passport.user,
@@ -35,49 +38,14 @@ router.put('/createcart', async (req, res, next) => {
     }
     res.json(lastOrder)
   } catch (error) {
-    console.error(error)
+    next(error)
   }
 })
 
 //GET api/orders/cart
 router.get('/cart', async (req, res, next) => {
-  const lastOrder = await Order.findOne({
-    where: {
-      userId: req.session.passport.user,
-      orderPlaced: false
-    },
-    include: [
-      {
-        model: Product,
-        attributes: ['id', 'itemName', 'price']
-      }
-    ],
-    order: [['updatedAt', 'DESC']]
-  })
-  res.send(lastOrder)
-})
-
-//PUT api/orders/:itemId (when you add item in single product view and increment quantity on cart at checkout)
-router.put('/:itemId', async (req, res, next) => {
-  let itemNum = req.params.itemId
-  const lastOrder = await Order.findOne({
-    where: {
-      userId: req.session.passport.user,
-      orderPlaced: false
-    },
-    order: [['updatedAt', 'DESC']]
-  })
-  const findItem = await Orderproduct.findOne({
-    where: {
-      orderId: lastOrder.id,
-      productId: itemNum
-    }
-  })
-  if (findItem) {
-    //increments the quantity
-    await findItem.increment('quantity')
-    //fetching the updated cart with the new quantity
-    const updatedCart = await Order.findOne({
+  try {
+    const lastOrder = await Order.findOne({
       where: {
         userId: req.session.passport.user,
         orderPlaced: false
@@ -90,68 +58,115 @@ router.put('/:itemId', async (req, res, next) => {
       ],
       order: [['updatedAt', 'DESC']]
     })
-    //here, we are returning a cart to get the updated(quantity) cart in the thunk
-    res.send(updatedCart)
-  } else {
-    //adds to cart with quantity(defaults to 1).
-    let newItem = await Orderproduct.findOrCreate({
+    res.send(lastOrder)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//PUT api/orders/:itemId (when you add item in single product view and increment quantity on cart at checkout)
+router.put('/:itemId', async (req, res, next) => {
+  try {
+    let itemNum = req.params.itemId
+    const lastOrder = await Order.findOne({
+      where: {
+        userId: req.session.passport.user,
+        orderPlaced: false
+      },
+      order: [['updatedAt', 'DESC']]
+    })
+    const findItem = await Orderproduct.findOne({
       where: {
         orderId: lastOrder.id,
         productId: itemNum
       }
     })
-    res.sendStatus(200)
+    if (findItem) {
+      //increments the quantity
+      await findItem.increment('quantity')
+      //fetching the updated cart with the new quantity
+      const updatedCart = await Order.findOne({
+        where: {
+          userId: req.session.passport.user,
+          orderPlaced: false
+        },
+        include: [
+          {
+            model: Product,
+            attributes: ['id', 'itemName', 'price']
+          }
+        ],
+        order: [['updatedAt', 'DESC']]
+      })
+      //here, we are returning a cart to get the updated(quantity) cart in the thunk
+      res.send(updatedCart)
+    } else {
+      //adds to cart with quantity(defaults to 1).
+      let newItem = await Orderproduct.findOrCreate({
+        where: {
+          orderId: lastOrder.id,
+          productId: itemNum
+        }
+      })
+      res.sendStatus(200)
+    }
+  } catch (error) {
+    next(error)
   }
 })
 
 //PUT api/orders/:itemId/decrement
 router.put('/:itemId/decrement', async (req, res, next) => {
-  let itemNum = req.params.itemId
-  const lastOrder = await Order.findOne({
-    where: {
-      userId: req.session.passport.user,
-      orderPlaced: false
-    },
-    order: [['updatedAt', 'DESC']]
-  })
-  const findItem = await Orderproduct.findOne({
-    where: {
-      orderId: lastOrder.id,
-      productId: itemNum
+  try {
+    let itemNum = req.params.itemId
+    const lastOrder = await Order.findOne({
+      where: {
+        userId: req.session.passport.user,
+        orderPlaced: false
+      },
+      order: [['updatedAt', 'DESC']]
+    })
+    const findItem = await Orderproduct.findOne({
+      where: {
+        orderId: lastOrder.id,
+        productId: itemNum
+      }
+    })
+    if (findItem.quantity === 1) {
+      await findItem.destroy()
+      const newCart = await Order.findOne({
+        where: {
+          userId: req.session.passport.user,
+          orderPlaced: false
+        },
+        include: [
+          {
+            model: Product,
+            attributes: ['id', 'itemName', 'price']
+          }
+        ],
+        order: [['updatedAt', 'DESC']]
+      })
+      res.send(newCart)
+    } else {
+      await findItem.decrement('quantity')
+      const newCart = await Order.findOne({
+        where: {
+          userId: req.session.passport.user,
+          orderPlaced: false
+        },
+        include: [
+          {
+            model: Product,
+            attributes: ['id', 'itemName', 'price']
+          }
+        ],
+        order: [['updatedAt', 'DESC']]
+      })
+      res.send(newCart)
     }
-  })
-  if (findItem.quantity === 1) {
-    await findItem.destroy()
-    const newCart = await Order.findOne({
-      where: {
-        userId: req.session.passport.user,
-        orderPlaced: false
-      },
-      include: [
-        {
-          model: Product,
-          attributes: ['id', 'itemName', 'price']
-        }
-      ],
-      order: [['updatedAt', 'DESC']]
-    })
-    res.send(newCart)
-  } else {
-    await findItem.decrement('quantity')
-    const newCart = await Order.findOne({
-      where: {
-        userId: req.session.passport.user,
-        orderPlaced: false
-      },
-      include: [
-        {
-          model: Product,
-          attributes: ['id', 'itemName', 'price']
-        }
-      ],
-      order: [['updatedAt', 'DESC']]
-    })
-    res.send(newCart)
+  } catch (error) {
+    next(error)
   }
 })
 
